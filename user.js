08a3eeaa76ee6d634d53026199fdd83e9e840b54
Noah Williamson <williamson.noah@gmail.com>
2017-11-18 18:28:20 -0500

    user profile
    this needs more work

---
+++ user.js
@@ -0,0 +1,57 @@
+/*
+ * user.js
+ * 
+ * Last edit: 11/18/17 Noah Williamson
+ *
+ */
+
+// USER PROFILE
+
+/*  GLOBALS  */
+
+// should be self explanatory, information for the user to fill out
+var fields = {
+
+  'name' : 'PLACEHOLDER',
+  'username' : 'USER',
+  'password' : 'PROTECTED',
+  'location' : 'LOCATION',
+  'bio' : 'PLACEHOLDER'
+
+};
+
+
+/*  FUNCTIONS */
+// these should be self explanatory as well
+
+recommend(){
+  // handle recommend
+}
+
+like(){
+  // handle 'liking' a pet
+}
+
+superLike(){
+  // handle the 'super like', i.e. prioritize when sending matches
+}
+
+dislike(){
+  // essentially we will just ignore the request here
+}
+
+// MAYBE: undo
+// i.e. go to previous pet profile card
+undo(){
+  // go back
+  // decrement our counter in which we keep track of the array of Objects
+}
+
+boost(){
+  // handle boosting
+}
+
+chat(){
+  // send email!
+}
+
