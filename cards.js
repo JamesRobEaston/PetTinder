b227e3ba592cb57aca6a5b92b0991ccf70070d50
JamesEaston <james.easton@stu.jefferson.kyschools.us>
2017-11-18 21:05:02 -0500

    Beginning of Sort

--- cards.js
+++ cards.js
@@ -5,3 +5,30 @@ var cards;
 function addCard(newCard){
 	card.push(newCard);
 }
+
+sortCards(){
+	//Alphabetic sort
+	cards.sort(function(a, b){
+        var x = a.animalType.toLowerCase();
+        var y = b.animalType.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+    });
+
+	let influence = 
+    cards.sort(function(a, b){
+        var x = a.type.toLowerCase();
+        var y = b.type.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+    });
+}
+
+//swaps two elements in cards
+swap(a, b){
+	let temp = cards[a];
+	cards[a] = cards[b];
+	cards[b] = temp;
+}
